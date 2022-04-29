53dda65e5b6b80db13ea6f31ee1779ce8badd661
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-04-29 16:11:03 +0200

    Add Player search page and display results
    This is still very rudimentary but it works.

--- player_lookup/urls.py
+++ player_lookup/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'player_lookup'
+
 urlpatterns = [
-    path('', views.PlayerLookupView.as_view(), name='player_lookup'),
+    path('', views.PlayerLookupView.as_view(), name='index'),
+    path('player_page/<str:player_tag>', views.PlayerPageView.as_view(),
+         name='player_page'),
 ]
--- player_lookup/views.py
+++ player_lookup/views.py
@@ -1,8 +1,40 @@
-from django.views.generic import FormView
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
 
 from .forms import PlayerLookupForm
+from .brawlstars_api import BrawlAPi
+
+brawl_api = BrawlAPi()
 
 
 class PlayerLookupView(FormView):
     template_name = 'player_lookup/player_lookup.html'
     form_class = PlayerLookupForm
+    success_url = 'player_lookup:player_page'
+
+    def form_valid(self, form):
+        """If the form is valid, redirect to the supplied URL."""
+        player_tag = form.cleaned_data['player_tag']
+        return HttpResponseRedirect(self.get_success_url(player_tag))
+
+    def get_success_url(self, player_tag) -> str:
+        """Return the URL to redirect to after processing a valid form."""
+        if not self.success_url:
+            raise ImproperlyConfigured("No URL to redirect to. "
+                                       "Provide a success_url.")
+        success_url = reverse_lazy(self.success_url, args=[player_tag])
+        return success_url
+
+
+class PlayerPageView(TemplateView):
+    template_name = 'player_lookup/player_page.html'
+
+    def get_context_data(self, **kwargs):
+        """Displays the stats of the player."""
+        player_tag = kwargs['player_tag']
+        player_data = brawl_api.get_player_stats(player_tag)
+        context = super().get_context_data(**kwargs)
+        context['player_data'] = player_data
+        return context
