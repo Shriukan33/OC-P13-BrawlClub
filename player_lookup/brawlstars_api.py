a919b9b7dd6f1ed66cb110bed6948c8be25ac631
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-05-06 21:51:39 +0200

    Improvement in Brawl Stars' API implementation
    Now only remove first char if it's indeed a #
    
    Add a function that gets the list of players tag in a club.

--- player_lookup/brawlstars_api.py
+++ player_lookup/brawlstars_api.py
@@ -13,16 +13,25 @@ class BrawlAPi:
         }
 
     def get_player_stats(self, player_tag: str) -> dict:
-        # We remove the # from the player tag
-        player_tag = player_tag[1:]
+        if player_tag.startswith("#"):
+            # We remove the # from the player tag
+            player_tag = player_tag[1:]
         url = f"{self.base_url}players/%23{player_tag}"
         response = requests.get(url, headers=self.headers)
         return response.json()
 
+    def get_player_stats_url(self, player_tag: str) -> str:
+        if player_tag.startswith("#"):
+            # We remove the # from the player tag
+            player_tag = player_tag[1:]
+        url = f"{self.base_url}players/%23{player_tag}"
+        return url
+
     def get_player_battlelog(self, player_tag: str) -> dict:
         """Get data from the player's last 24 matches"""
-        # We remove the # from the player tag
-        player_tag = player_tag[1:]
+        if player_tag.startswith("#"):
+            # We remove the # from the player tag
+            player_tag = player_tag[1:]
         url = f"{self.base_url}players/%23{player_tag}/battlelog"
         response = requests.get(url, headers=self.headers)
         return response.json()
@@ -31,16 +40,26 @@ class BrawlAPi:
         data = self.get_player_stats(player_tag)
         return data["club"]
 
-    def get_club_members(self, club_tag: str) -> list:
-        # We remove the # from the club tag
-        club_tag = club_tag[1:]
+    def get_club_members(self, club_tag: str) -> dict:
+        if club_tag.startswith("#"):
+            # We remove the # from the club tag
+            club_tag = club_tag[1:]
         url = f"{self.base_url}clubs/%23{club_tag}/members"
         response = requests.get(url, headers=self.headers)
         return response.json()
 
+    def get_club_members_tag_list(self, club_tag: str) -> list:
+        """Returns a list of the club members' tags."""
+        club_members = self.get_club_members(club_tag)
+        club_members_tag_list = []
+        for member in club_members["items"]:
+            club_members_tag_list.append(member["tag"])
+        return club_members_tag_list
+
     def get_club_information(self, club_tag: str) -> dict:
-        # We remove the # from the club tag
-        club_tag = club_tag[1:]
+        if club_tag.startswith("#"):
+            # We remove the # from the club tag
+            club_tag = club_tag[1:]
         url = f"{self.base_url}clubs/%23{club_tag}"
         response = requests.get(url, headers=self.headers)
         return response.json()
