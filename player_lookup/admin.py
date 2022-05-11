2d92a85c5829850ca64e995117a2f106601297f8
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-05-11 23:28:44 +0200

    Add to admin panel the remaining models

--- player_lookup/admin.py
+++ player_lookup/admin.py
@@ -15,6 +15,34 @@ class ClubAdmin(admin.ModelAdmin):
     search_fields = ('club_tag', 'club_name')
 
 
+class BrawlMapAdin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+class BrawlerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('mode', 'map_played', 'date')
+    list_filter = ('mode', 'map_played')
+    search_fields = ('mode', 'map_played')
+
+
+class MatchIssueAdmin(admin.ModelAdmin):
+    list_display = ("player", "brawler", "has_won", "trophies_won")
+    list_filter = ("player", "brawler", "has_won")
+    search_fields = ("player", "brawler")
+
+
 admin.site.register(models.Player, PlayerAdmin)
 admin.site.register(models.Club, ClubAdmin)
+admin.site.register(models.BrawlMap, BrawlMapAdin)
+admin.site.register(models.Brawler, BrawlerAdmin)
+admin.site.register(models.Match, MatchAdmin)
+admin.site.register(models.MatchIssue, MatchIssueAdmin)
 admin.site.site_header = "Brawl Club Admin"
