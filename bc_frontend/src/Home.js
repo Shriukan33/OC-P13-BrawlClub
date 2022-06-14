dd7af0cad3766065a79d2b2a729d823b01ecd111
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-06-14 12:26:50 +0200

    Add alt to logo

--- bc_frontend/src/Home.js
+++ bc_frontend/src/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 const Home = () => {
   return (
     <main className='HomePage'>
-        
+        <img src="/images/brawlclub_logo.png" alt="Brawlclub logo"></img>
     </main>
   )
 }
