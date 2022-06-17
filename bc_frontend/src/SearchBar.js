52fff9e19deb131e0d03e9abda3c1d4263009c6a
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-06-17 17:36:08 +0200

    Make navbar's searchbar functional to look for players

--- bc_frontend/src/SearchBar.js
+++ bc_frontend/src/SearchBar.js
@@ -1,16 +1,35 @@
 import React from 'react'
-import styles from "./SearchBar.css"
+import "./SearchBar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState('')
+  const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Need to check if this is a valid player or club
+    // Check what sort of entity it is (Tag is either player or club)
+    navigate(`/player/${searchValue}`)
+    setSearchValue('')
+  }
+  const handleChange = (e) => {
+    let new_value = e.target.value
+    new_value = new_value.replace('#', '')
+    setSearchValue(new_value) 
+  }
+
   return (
-    <div className="search-box">
-        <button className="btn-search">
+    <form className="search-box" onSubmit={handleSubmit}>
+        <button type="button" className="btn-search">
             <FontAwesomeIcon icon={faSearch} />
         </button>
-        <input type="text" className="input-search" placeholder="Enter a player or a club tag"/>
-    </div>
+        <input type="text" className="input-search" placeholder="Enter a player or a club tag"
+          value={searchValue}
+          onChange={handleChange}/>
+    </form>
   )
 }
 
