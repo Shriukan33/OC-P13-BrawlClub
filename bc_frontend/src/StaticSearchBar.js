46becf12fffc70614c3a4cc8a5f54c533e453273
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-06-18 14:43:10 +0200

    Make home searchbar effectively search

--- bc_frontend/src/StaticSearchBar.js
+++ bc_frontend/src/StaticSearchBar.js
@@ -2,12 +2,30 @@ import React from 'react'
 import styles from './StaticSearchBar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const StaticSearchBar = () => {
+  const [searchValue, setSearchValue] = useState('')
+  const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Need to check if this is a valid player or club
+    // Check what sort of entity it is (Tag is either player or club)
+    navigate(`/player/${searchValue}`)
+    setSearchValue('')
+  }
+  const handleChange = (e) => {
+    let new_value = e.target.value
+    new_value = new_value.replace('#', '')
+    setSearchValue(new_value) 
+  }
   return (
-    <form method='POST' action='#' onSubmit={(e) => e.preventDefault()}
+    <form method='POST' action='#' onSubmit={handleSubmit}
     className="d-flex flex-row justify-content-center">
-        <input type="text" className={styles["input-search"]+" d-flex"} placeholder="Enter a player or a club tag"/>
+        <input type="text" className={styles["input-search"]+" d-flex"}
+        placeholder="Enter a player or a club tag"
+        onChange={handleChange}/>
         <button className={styles["btn-search"] + " px-3"}>
             <FontAwesomeIcon icon={faSearch} />
         </button>
