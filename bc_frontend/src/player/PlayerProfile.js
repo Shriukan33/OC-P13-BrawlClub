6067043c53c89e02a856441ecd5ced2ad83248f1
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-06-24 20:19:50 +0200

    Redesign layout of icons in PlayerProfile

--- bc_frontend/src/player/PlayerProfile.js
+++ bc_frontend/src/player/PlayerProfile.js
@@ -3,7 +3,7 @@ import "./PlayerProfile.css"
 
 const PlayerProfile = () => {
   return (
-    <div className="PlayerProfile d-flex flex-column p-2 w-100">
+    <div className="PlayerProfile d-flex flex-column p-2 w-100 h-100">
         <div className='d-flex flex-row'>
             <div id="PlayerProfilePicture">
                 <img src="/images/Player/Portrait_placeholder.png" alt="Player profile pic"/>
@@ -14,18 +14,18 @@ const PlayerProfile = () => {
                 <span>#ABCDE123</span>
             </div>
         </div>
-        <div id="PlayerProfileMetrics" className="d-flex flex-row mt-2">
-            <div id="PlayerLevel" className="d-flex mx-2 my-auto ">
-                <span className="mright-2 my-auto">Level&nbsp;:</span>
+        <div id="PlayerProfileMetrics" className="d-flex flex-row my-auto justify-content-evenly">
+            <div id="PlayerLevel" className="d-flex flex-column m-auto ">
+                <span className="mright-2 m-auto">Level</span>
                 <div className="level-badge">
                     <img className="level-icon " src="/images/Player/level.png" alt="Player level" />
                     <span>150</span>
                 </div>
             </div>
-            <div id="PlayerTotalTrophies" className="d-flex mx-2 my-auto flex-wrap">
-                <span className="m-auto">Total of Trophies&nbsp;:</span>
+            <div id="PlayerTotalTrophies" className="d-flex flex-column m-auto flex-wrap">
+                <span className="m-auto ">Trophies</span>
                 <div className="m-auto">
-                    <img className="mleft-2" src="/images/Player/trophy.png" alt="Player trophies" />
+                    <img src="/images/Player/trophy.png" alt="Player trophies" />
                     <span className="m-auto">28250</span>
                 </div>
             </div>
