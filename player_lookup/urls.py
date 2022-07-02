62b4a93bb56accf0baa1380e55f8aa0917f03a0a
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-07-02 15:09:56 +0200

    Remove CBV from initial project

--- player_lookup/urls.py
+++ player_lookup/urls.py
@@ -4,8 +4,5 @@ from . import views
 app_name = 'player_lookup'
 
 urlpatterns = [
-    path('', views.PlayerLookupView.as_view(), name='index'),
-    path('player_page/<str:player_tag>', views.PlayerPageView.as_view(),
-         name='player_page'),
     path('club_members/<str:club_tag>', views.update_club_members)
 ]
--- player_lookup/views.py
+++ player_lookup/views.py
@@ -6,60 +6,14 @@ from typing import Tuple
 import dateutil.parser
 import httpx
 from asgiref.sync import async_to_sync
-from django.core.exceptions import ImproperlyConfigured
-from django.http import HttpResponseRedirect, JsonResponse
-from django.urls import reverse_lazy
-from django.views.generic import FormView, TemplateView
+from django.http import JsonResponse
 
 from . import models
 from .brawlstars_api import BrawlAPi
-from .forms import PlayerLookupForm
 
 brawl_api = BrawlAPi()
 
 logger = logging.getLogger("django")
-
-
-class PlayerLookupView(FormView):
-    template_name = "player_lookup/player_lookup.html"
-    form_class = PlayerLookupForm
-    success_url = "player_lookup:player_page"
-
-    def form_valid(self, form):
-        """If the form is valid, redirect to the supplied URL."""
-        player_tag = form.cleaned_data["player_tag"]
-        return HttpResponseRedirect(self.get_success_url(player_tag))
-
-    def get_success_url(self, player_tag) -> str:
-        """Return the URL to redirect to after processing a valid form."""
-        if not self.success_url:
-            raise ImproperlyConfigured(
-                "No URL to redirect to. " "Provide a success_url."
-            )
-        success_url = reverse_lazy(self.success_url, args=[player_tag])
-        return success_url
-
-
-class PlayerPageView(TemplateView):
-    template_name = "player_lookup/player_page.html"
-
-    def get_context_data(self, **kwargs):
-        """Displays the stats of the player."""
-        player_tag = kwargs["player_tag"]
-        player_data = self.get_number_of_trophies_in_club_war(player_tag)
-        context = super().get_context_data(**kwargs)
-        context["player_data"] = player_data
-        return context
-
-    def get_number_of_trophies_in_club_war(self, player_tag: str) -> int:
-        """Returns the number of trophies in club war if any in
-        the last 24 matches."""
-        player_battlelog = brawl_api.get_player_battlelog(player_tag)
-        number_of_trophies = 0
-        for match in player_battlelog["items"]:
-            if match["battle"]["type"] == "teamRanked":
-                number_of_trophies += match["battle"].get("trophyChange", 0)
-        return number_of_trophies
 
 
 @async_to_sync
