3f7ae4d15e836eac4850c1f446c51e7511566dea
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-07-22 17:19:36 +0200

    Update Leaderboard and LeaderboardLine components
    This allows these component to adapt to the nature
    of the data they are given (Players or clubs)

--- bc_frontend/src/home/Leaderboard.js
+++ bc_frontend/src/home/Leaderboard.js
@@ -4,13 +4,14 @@ import LeaderboardLine from './LeaderboardLine'
 
 const Leaderboard = ({title, topEntities, path}) => {
 
+  
   return (
     <div className="leaderboard-frame d-flex flex-column col-10 col-md-4 my-3 mx-5 px-3">
       <header className="mx-auto mt-2">{title}</header>
       <div className="d-flex flex-column p-1 leaderboard-body mb-2">
         {topEntities.length ?
           topEntities.map(entity => (
-            <LeaderboardLine key={entity.id} entity={entity} path={path} />
+            <LeaderboardLine key={entity.player_tag ?? entity.club_tag} entity={entity} path={path} />
           ))
           :
           <div className="d-flex justify-content-center">No player or club found</div>
--- bc_frontend/src/home/LeaderboardLine.js
+++ bc_frontend/src/home/LeaderboardLine.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 import './LeaderboardLine.css'
 
 const LeaderboardLine = ({ entity, path }) => {
+  entity.name = entity.player_name ?? entity.club_name
+  entity.id = entity.player_tag ?? entity.club_tag
+  entity.id = entity.id.replace("#", "")
+  entity.metric = entity.brawlclub_rating ?? entity.avg_bcr
+  entity.metric = entity.metric.toFixed(2)
   return (
     <div className='d-flex mt-1 justify-content-between'>
         <div className='leaderboard-line-name'>
