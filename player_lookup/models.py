af42eb783b9e5052a0058f2f528d3d9e4bba96b6
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-10-07 11:40:11 +0200

    Lengthen several Charfields fields limits
    player / club tags :
    I thought that 9 was the maximum, but found many occurrences of 10.
    To give me some more room for the future, I increased the fields' length
    to 15.
    
    club description :
    I thought that 100 was the maximum, but found many occurrences of more.
    To give me some more room for the future, I increased the fields' length
    to 200.
    
    Match ID:
    Because Player tag is longer than expected, I increases it as well

--- player_lookup/models.py
+++ player_lookup/models.py
@@ -20,7 +20,7 @@ def limit_number_of_players(value):
 
 class Player(models.Model):
 
-    player_tag = models.CharField(max_length=9, primary_key=True)
+    player_tag = models.CharField(max_length=15, primary_key=True)
     player_name = models.CharField(max_length=50)
     trophy_count = models.IntegerField(default=0)
     club = models.ForeignKey(
@@ -217,9 +217,9 @@ class PlayerHistory(models.Model):
 class Club(models.Model):
     """Clubs are groups of players up to 30 persons"""
 
-    club_tag = models.CharField(max_length=9, primary_key=True)
+    club_tag = models.CharField(max_length=15, primary_key=True)
     club_name = models.CharField(max_length=50)
-    club_description = models.CharField(max_length=100, blank=True)
+    club_description = models.CharField(max_length=200, blank=True)
     club_type = models.CharField(max_length=20, default="open")
     required_trophies = models.IntegerField(default=0)
     trophies = models.IntegerField(default=0)
@@ -279,7 +279,7 @@ class Match(models.Model):
 
     # The match id is build with the following format:
     # <star_player_tag_without_#><timestamp_in_epoch_seconds>
-    match_id = models.CharField(max_length=18, primary_key=True)
+    match_id = models.CharField(max_length=30, primary_key=True)
     # Brawlball, Gem grab, Knockout ...
     mode = models.CharField(max_length=20)
     # Map
