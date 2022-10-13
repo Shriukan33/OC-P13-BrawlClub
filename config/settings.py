30839245417b521c111cb589fcbb3fe6359751c5
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-10-13 10:17:00 +0200

    Remove un-needed rollbar initialization code

--- config/settings.py
+++ config/settings.py
@@ -11,12 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import rollbar
 import os
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
-rollbar.init(os.environ.get('ROLLBAR_ACCESS_TOKEN'), '')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
