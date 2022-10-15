e8ad293c58f03aa57bde24a040a9836539675d11
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-10-15 18:18:34 +0200

    Add names to url patterns

--- player_lookup/urls.py
+++ player_lookup/urls.py
@@ -7,12 +7,36 @@ app_name = "player_lookup"
 urlpatterns = []
 
 drf_patterns = [
-    path("api/club-finder/results", views.ClubFinderResultsView.as_view()),
-    path("api/club-members/<str:club_tag>", views.ClubMembersView.as_view()),
-    path("api/leaderboard/<str:entity>/", views.LeaderBoardView.as_view()),
-    path("api/player/areagraph/<str:player_tag>", views.PlayerAreaGraphView.as_view()),
-    path("api/search/<str:tag>", views.SearchUnknownEntityView.as_view()),
-    path("api/<str:entity>/<str:tag>", views.SingleEntityView.as_view()),
+    path(
+        "api/club-finder/results",
+        views.ClubFinderResultsView.as_view(),
+        name="club_finder_results",
+    ),
+    path(
+        "api/club-members/<str:club_tag>",
+        views.ClubMembersView.as_view(),
+        name="club_members",
+    ),
+    path(
+        "api/leaderboard/<str:entity>/",
+        views.LeaderBoardView.as_view(),
+        name="leaderboard",
+    ),
+    path(
+        "api/player/areagraph/<str:player_tag>",
+        views.PlayerAreaGraphView.as_view(),
+        name="player_areagraph",
+    ),
+    path(
+        "api/search/<str:tag>",
+        views.SearchUnknownEntityView.as_view(),
+        name="search_unknown",
+    ),
+    path(
+        "api/<str:entity>/<str:tag>",
+        views.SingleEntityView.as_view(),
+        name="search_specific",
+    ),
 ]
 
 urlpatterns += format_suffix_patterns(drf_patterns)
