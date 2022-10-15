2de210917fc301ca5b535bb6ee987b406e716f67
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-10-15 17:34:06 +0200

    Delete development urls
    These urls were used for development and are no longer needed, and
    even harmful in a production context, as they aren't protected.

--- player_lookup/urls.py
+++ player_lookup/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 app_name = "player_lookup"
 
-urlpatterns = [
-    path("update_clubs/", views.update_all_clubs, name="update_clubs"),
-    path("club_members/<str:club_tag>", views.update_club_members),
-]
+urlpatterns = []
 
 drf_patterns = [
     path("api/club-finder/results", views.ClubFinderResultsView.as_view()),
