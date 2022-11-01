1d5123326ff9a1417937eb899272d77d667d399a
zaq <zaqrashyy7@gmail.com>
2022-11-01 17:44:42 +0100

    minor change to leaderboard frame

--- bc_frontend/src/home/Home.js
+++ bc_frontend/src/home/Home.js
@@ -76,7 +76,7 @@ const Home = () => {
                <p>The about and how to use goes here</p>
                 </div>
 
-                <section className="d-flex flex-wrap col-10 justify-content-center mx-auto h-460px">
+                <section className="d-flex flex-wrap col-10 justify-content-center mx-auto plo">
           <div className="leaderboard-frame d-flex flex-column col-12 col-md-4 my-3 mx-5 px-3">
             <Leaderboard
               title="Player Rating Leaderboard"
@@ -92,10 +92,37 @@ const Home = () => {
         </section>
 
 
-        {/* <section className="playlist">
+        <section className="playlist">
 
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/G_wxQe2Litw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      {/* <img class="d-block w-100" src="https://www.youtube.com/watch?v=G_wxQe2Litw" alt="First slide"></img> */}
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide"></img>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
         
-        </section> */}
+        </section>
                 </section>
     </main>
   )
